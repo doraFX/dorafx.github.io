171cd479913860221d32babb9d693c7390b40ae1
dorafx <37041478+doraFX@users.noreply.github.com>
2021-08-02 17:23:04 +0800

    更新

---
+++ js/portfolio.js
@@ -0,0 +1,89 @@
+function closebutton() {
+	$(".single").hide();
+}
+
+function closebuttonvideo() {
+	$(".single-video").hide();
+}
+
+function p1() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/01.webp");
+}
+
+function p2() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/02.webp");
+}
+
+function p3() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/03.webp");
+}
+
+function p4() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/04.webp");
+}
+
+function p5() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/05.webp");
+}
+
+function p6() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/06.webp");
+}
+
+function p7() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/07.webp");
+}
+
+function p8() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/08.webp");
+}
+
+function p9() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/09.webp");
+}
+
+function p10() {
+	window.location.href = "floatbeacon.html";
+}
+
+function p11() {
+	$(".single-video").show();
+	$("#SVideo").attr("src", "https://demoreel-video.vercel.app/2035.mp4");
+	$("#SVideo").attr("poster", "images/works/11.webp");
+}
+
+function p12() {
+	$(".single-video").show();
+	$("#SVideo").attr("src", "https://demoreel-video.vercel.app/DEMOREEL_x264.mp4");
+	$("#SVideo").attr("poster", "images/works/12.webp");
+}
+
+function p13() {
+	$(".single-video").show();
+	$("#SVideo").attr("src", "https://demoreel-video.vercel.app/third-party-assets.mp4");
+	$("#SVideo").attr("poster", "images/works/13.webp");
+}
+
+function p14() {
+	window.location.href = "gallery/index.html";
+}
+
+function p15() {
+	$(".single-video").show();
+	$("#SVideo").attr("src", "https://demoreel-video.vercel.app/JNU60.mp4");
+	$("#SVideo").attr("poster", "images/works/15.webp");
+}
+
+function p16() {
+	$(".single").show();
+	$("#SImg").attr("src", "images/works/16.webp");
+}
